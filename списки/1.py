7b0d40561caed1dbcf232bcd6384dee3aa8dd4b1
Soluyanov-Andrey <a--b--s@yandex.ru>
2018-07-13 17:57:09 +0300

    13
    45

--- списки/1.py
+++ списки/1.py
@@ -63,6 +63,76 @@ if('сто1' in story_count):
 l=list(story_count.keys())
 l.sort()
 print(l)
+------------------------------------------------------------------------------------------
+"""
+with open(r'C:\1\text.txt') as inf:
+   
+    
+
+
+
+    for line in inf:
+        line=line.lower()
+        line1=line1+ line.strip()+" "
+
+l=line1.split(' ')
+print(l)
+b = [5, 10, 1]
+
+s=sorted(l)
+
+print(s)
+
+
+s=my_string.lower()
+
+print(my_string)
+
+b=s.split(' ')
+print(b)
+
+ks=sorted(b)
+
+
+
+
+"""
+
+b1=[]
+file = r'C:\1\text.txt'
+b=open(file).read().split('\n')
+for line in b:
+    line=line.lower()
+    b1=b1+line.split(' ')
+
+story_count = {'а':12, 'у':14, 'б':10, 'к':49, 'm':13, 'e':49, 'я':3, 'в':6,'t':24 }
+
+
+def keywithmaxval(d):
+   
+    v = list(d.values())
+# подав {'а':12, 'у':14, 'б':10, 'к':49, 'm':13, 'e':49, 'я':3, 'в':6,'t':24 }
+# [12, 14, 10, 49, 13, 49, 3, 6, 24]
+    print(v)
+    k = list(d.keys())
+# подав {'а':12, 'у':14, 'б':10, 'к':49, 'm':13, 'e':49, 'я':3, 'в':6,'t':24 }
+# получим ['а', 'у', 'б', 'к', 'm', 'e', 'я', 'в', 't']
+
+    return k[v.index(max(v))]
+
+
+
+print(b1)
+b1=set(b1)
+ks=sorted(b1)
+
+print(keywithmaxval(story_count))
+
+
+
+
+
+
 
 
 
